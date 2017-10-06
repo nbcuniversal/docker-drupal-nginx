3f594eec2d47d4216b12122c9c08fcd1f5c65370
Vaughan Jones <vaughanlewisjones@icloud.com>
2017-10-06 13:51:35 +0100

    Default URLs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -e
 
 if [ -d /etc/template.d ]; then
-  if [ -z "$siteInternalUrl" ]; then
-    export siteInternalUrl="dimpl.cluster.local"
+  if [[ -z "$siteInternalUrl" ]]; then
+    export siteInternalUrl="drupal.cluster.local"
   fi
+
+  if [[ -z "$siteExternalUrl" ]]; then
+    export siteExternalUrl="drupal.cluster.ext"
+  fi
+  
   echo "==> Generating configuration ${siteInternalUrl}"
   echo "  + Using origin ${siteInternalUrl}"
   echo "  + Using external domain ${siteExternalUrl}"
